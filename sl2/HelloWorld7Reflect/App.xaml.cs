62d445e2b44af0a9050dbe844d09a5e910e01f4d
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-06-16 10:08:09 +0000

    Updated example for SL2 beta 2

--- sl2/HelloWorld7Reflect/App.xaml.cs
+++ sl2/HelloWorld7Reflect/App.xaml.cs
@@ -4,20 +4,6 @@ using System.IO;
 using System.Windows;
 using System.Windows.Markup;
 using System.Windows.Browser;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Resources;
-using System.Windows.Shapes;
-
-namespace System.Collections
-{
-    public class ArrayList
-    {
-    }
-}
 
 namespace Sl2
 {
@@ -30,31 +16,15 @@ namespace Sl2
             this.Startup += this.Application_Startup;
             this.Exit += this.Application_Exit;
             this.UnhandledException += this.Application_UnhandledException;
-
-            //Uri uri = new Uri("App.xaml");
-            //System.Windows.Application.LoadComponent(this, uri);
-
-            //this.StartupUri = new System.Uri("Window1.xaml", System.UriKind.Relative);
-            //InitializeComponent();
         }
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            //string xaml =
-            //Page page = (Page)XamlReader.Load("Page.xaml");
             Uri uri = new Uri("Page.xaml", UriKind.Relative);
-            //StreamResourceInfo stream = GetResourceStream(uri);
-            //StreamReader reader = new StreamReader(stream.Stream);
-            //string xaml = reader.ReadToEnd();
-            //IsolatedStorageFile file = IsolatedStorageFile.GetUserStoreForApplication();
-            //HtmlPage.Window.Alert(file.FileExists("/Page.xaml").ToString());
-            //HtmlPage.Window.Alert(file.FileExists("/Pag2e.xaml").ToString());
 
             // Load the main control
             Page page = new Page();
-            Button button = new Button();
             System.Windows.Application.LoadComponent(page, uri);
-            //(Page)XamlReader.Load(xaml, true);
             RootVisual = page;
         }
 
