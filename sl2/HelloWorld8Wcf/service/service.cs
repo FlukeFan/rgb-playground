f1e7ffa2827f7b11a94223cc16315ab46756b051
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-06-17 10:35:39 +0000

    Separated domain and service classes.

--- sl2/HelloWorld8Wcf/service/service.cs
+++ sl2/HelloWorld8Wcf/service/service.cs
@@ -2,55 +2,10 @@
 using System;
 using System.ServiceModel;
 
+using Demo.Domain;
+
 namespace Demo.Services
 {
-
-    [Serializable]
-    public class Composite1
-    {
-        private string _value;
-        private Composite2 _c2;
-
-        public string Value
-        {
-            get { return _value; }
-            set { _value = value; }
-        }
-
-        public Composite2 C2
-        {
-            get { return _c2; }
-            set { _c2 = value; }
-        }
-    }
-
-    [Serializable]
-    public class Composite2
-    {
-        private int _value;
-
-        public int Value
-        {
-            get { return _value; }
-            set { _value = value; }
-        }
-    }
-
-    [Serializable]
-    public class CustomException // : ApplicationException
-    {
-        private Composite1 _c1;
-
-        public CustomException(Composite1 c1)
-        {
-            _c1 = c1;
-        }
-
-        public Composite1 C1
-        {
-            get { return _c1; }
-        }
-    }
 
     [ServiceContract()]
     public interface ITestService
