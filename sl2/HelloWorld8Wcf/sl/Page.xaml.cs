5114413f37d29555995bfe51778b5fa3172a4cb3
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-06-17 12:53:10 +0000

    Added hack to get service calling (no value returned yet).

--- sl2/HelloWorld8Wcf/sl/Page.xaml.cs
+++ sl2/HelloWorld8Wcf/sl/Page.xaml.cs
@@ -14,16 +14,33 @@ namespace Sl2
     public class Page : UserControl
     {
 
+        private ServiceClient client;
+
         public Page()
         {
+            client = new ServiceClient();
+            client.GetC1Completed += new EventHandler<GetC1CompletedEventArgs>(client_GetC1Completed);
         }
 
         private void btnSend_Click(object sender, RoutedEventArgs e)
         {
+            client.GetC1Async();
             TextBlock tb = (TextBlock)FindName("textResponse");
-            tb.Text = "Hello world 5 at " + DateTime.Now.ToString() + ":" + DateTime.Now.TimeOfDay.ToString() + "\n";
-            tb.Text += "Hello world 5 at " + DateTime.Now.ToString() + ":" + DateTime.Now.TimeOfDay.ToString() + "\n";
-            tb.Text += "Hello world 5 at " + DateTime.Now.ToString() + ":" + DateTime.Now.TimeOfDay.ToString() + "\n";
+            tb.Text = "called";
+        }
+
+        private void client_GetC1Completed(object sender, GetC1CompletedEventArgs e)
+        {
+            TextBlock tb = (TextBlock)FindName("textResponse");
+            if (e.Error != null)
+            {
+                tb.Text = e.Error.Message;
+                throw e.Error;
+            }
+            else
+            {
+                tb.Text = e.Result.Value;
+            }
         }
 
     }
