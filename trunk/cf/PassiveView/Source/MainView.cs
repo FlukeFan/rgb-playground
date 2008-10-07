536ad169da597355e7da50635f78e8f81c5984c3
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-10-07 21:03:46 +0000

    Added first form.

---
+++ trunk/cf/PassiveView/Source/MainView.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cf.PassiveView.Source
+{
+    public partial class MainView : Form
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
