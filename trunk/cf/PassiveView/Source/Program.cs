1dbad2356ccc04cc1852df526f5d18cbdc26b0be
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-10-07 14:08:26 +0000

    Added first source file.

---
+++ trunk/cf/PassiveView/Source/Program.cs
@@ -0,0 +1,19 @@
+
+using System.Windows.Forms;
+
+namespace Cf.PassiveView
+{
+
+    public static class Program
+    {
+
+        [MTAThread]
+        public static void Main()
+        {
+            MessageBox.Show();
+        }
+
+    }
+
+}
+
