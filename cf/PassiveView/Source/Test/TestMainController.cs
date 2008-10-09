714b90b7aa9416ddc9e7f28d1989ba01daa88826
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-10-09 18:54:54 +0000

    Added first test.

---
+++ cf/PassiveView/Source/Test/TestMainController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace Cf.PassiveView.Source.Test
+{
+
+    [TestFixture]
+    public class TestMainController
+    {
+
+        [Test]
+        public void Test_That_TestIsRun()
+        {
+            Assert.Fail("Got to here");
+        }
+
+    }
+
+}
