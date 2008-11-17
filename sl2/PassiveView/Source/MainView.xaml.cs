e09d81a19f07571c9d6067758ee40d0d32b03234
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-11-17 16:34:13 +0000

    Fleshed out view class to expose controls.

--- sl2/PassiveView/Source/MainView.xaml.cs
+++ sl2/PassiveView/Source/MainView.xaml.cs
@@ -14,8 +14,22 @@ namespace Sl.PassiveView
     public class MainView : UserControl
     {
 
+        public Button ShowMessage;
+        public TextBlock Message;
+        public ComboBox ColourSelection;
+        public Button HideMessage;
+
         public MainView()
         {
+            Loaded += new RoutedEventHandler(UserControl_Loaded);
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowMessage = (Button)FindName("ShowMessage");
+            Message = (TextBlock)FindName("Message");
+            ColourSelection = (ComboBox)FindName("ColourSelection");
+            HideMessage = (Button)FindName("HideMessage");
         }
 
     }
