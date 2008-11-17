aff5be186e306d5c8ef5953c6ef1d47c1062f979
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-11-17 12:12:41 +0000

    Added startup view to contain sample's controls.

--- trunk/sl2/PassiveView/Source/App.xaml.cs
+++ trunk/sl2/PassiveView/Source/App.xaml.cs
@@ -14,25 +14,17 @@ namespace Sl.PassiveView
         public App()
         {
             this.Startup += this.Application_Startup;
-            this.Exit += this.Application_Exit;
             this.UnhandledException += this.Application_UnhandledException;
         }
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            MessageBox.Show("Hello");
-/*            Uri uri = new Uri("Page.xaml", UriKind.Relative);
-
-            // Load the main control
-            Page page = new Page();
-            System.Windows.Application.LoadComponent(page, uri);
-            RootVisual = page;*/
+            MainView mainView = new MainView();
+            Uri uri = new Uri("MainView.xaml", UriKind.Relative);
+            Application.LoadComponent(mainView, uri);
+            RootVisual = mainView;
         }
 
-        private void Application_Exit(object sender, EventArgs e)
-        {
-
-        }
         private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
         {
             HtmlPage.Window.Alert(e.ExceptionObject.ToString());
