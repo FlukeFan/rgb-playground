79dbc3fe0ac23dd320d47f9f8b7b59d3a62bc8ab
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-11-21 19:15:02 +0000

    Added example to create criteria.

--- 3.5/Extension/Run.cs
+++ 3.5/Extension/Run.cs
@@ -1,7 +1,10 @@
 
 using System;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
+
+using NHibernate.Criterion;
 
 namespace Demo
 {
@@ -11,18 +14,48 @@ namespace Demo
         public string Name { get; protected set; }
     }
 
+    public static class DetachedCriteriaExtension
+    {
+
+        public static DetachedCriteria Add<T>(this DetachedCriteria criteria, Expression<Func<T, bool>> e)
+        {
+            BinaryExpression be = (BinaryExpression)e.Body;
+            MemberExpression me = (MemberExpression)be.Left;
+            ConstantExpression ce = (ConstantExpression)be.Right;
+            criteria.Add(NHibernate.Criterion.Expression.Eq(me.Member.Name, ce.Value));
+            return criteria;
+        }
+
+    }
+
     public class Demo
     {
 
-        public static void Test<T>(Func<T, bool> func)
+        /*public static void Test<T>(Func<T, bool> func)
         {
-            Console.WriteLine(typeof(T));
+            Console.WriteLine(func);
+        }*/
+
+        public static void Test<T>(Expression<Func<T, bool>> e)
+        {
+            BinaryExpression be = (BinaryExpression)e.Body;
+            ConstantExpression ce = (ConstantExpression)be.Right;
+            Console.WriteLine(ce.Value.GetType());
+        }
+
+        public static void PropertyOf<T>(Expression<Func<T, object>> e)
+        {
         }
 
         [STAThread]
         public static void Main()
         {
             Test((Person p) => p.Name == "test");
+            DetachedCriteria criteria =
+                DetachedCriteria.For<Person>();
+            //criteria.Add(NHibernate.Criterion.Expression.Eq("Name", "test"));
+            criteria.Add((Person p) => p.Name == "test");
+            Console.WriteLine(criteria);
         }
 
     }
