ca40e088c5d24b8ab1fae4c4982af613fa4b3d33
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-11-26 10:35:19 +0000

    Added updated results location.

--- ccnet/SVNNhLambdaExtensions_trunk_CleanGet.bat
+++ ccnet/SVNNhLambdaExtensions_trunk_CleanGet.bat
@@ -2,5 +2,3 @@
 rmdir /S /Q "c:\build\NhLambdaExtensions"
 
 svn export --force http://nhlambdaextensions.googlecode.com/svn/trunk c:\build\NhLambdaExtensions
-
-mkdir c:\build\NhLambdaExtensions\results
