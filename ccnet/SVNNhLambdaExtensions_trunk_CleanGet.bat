45d51ba7b5ebe7c45289e617131355483a7c5fd4
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-11-26 09:27:00 +0000

    Added NhLambdaExtensions.

---
+++ ccnet/SVNNhLambdaExtensions_trunk_CleanGet.bat
@@ -0,0 +1,6 @@
+
+rmdir /S /Q "c:\build\NhLambdaExtensions"
+
+svn export --force http://nhlambdaextensions.googlecode.com/svn/trunk c:\build\NhLambdaExtensions
+
+mkdir c:\build\NhLambdaExtensions\results
