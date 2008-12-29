25d9cd58a398fb124e752a29937db30786405e7c
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2008-12-29 10:32:23 +0000

    Added service method to return an object graph.

--- sl2/HelloWorld8Wcf/service/domain.cs
+++ sl2/HelloWorld8Wcf/service/domain.cs
@@ -1,10 +1,59 @@
 
 using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 
 namespace Demo.Domain
 {
+
+    public enum PersonGender
+    {
+        Male = 1,
+        Female = 2,
+    }
+
+    [Serializable]
+    public class Person
+    {
+        protected Person() { }
+
+        public string Name { get; protected set; }
+        public int Age { get; protected set; }
+        public PersonGender Gender { get; protected set; }
+        public Person Father { get; protected set; }
+        protected IList<Person> Children { get; set; }
+        public IEnumerable<Person> ChildrenEnumeration { get { return Children; } }
+
+        public static Person CreatePerson()
+        {
+            Person person = new Person();
+            person.Children = new List<Person>();
+            return person;
+        }
+
+        public Person SetName(string name) { Name = name; return this; }
+        public Person SetAge(int age) { Age = age; return this; }
+        public Person SetGender(PersonGender gender) { Gender = gender; return this; }
+        public Person SetFather(Person father) { Father = father; return this; }
+        public Person AddChild(Person child) { Children.Add(child); return this; }
+
+        public void Throw()
+        {
+            throw new NameNotUniqueException(this);
+        }
+    }
+
+    [Serializable]
+    public class NameNotUniqueException : ApplicationException
+    {
+        public NameNotUniqueException(Person duplicatePerson)
+        {
+            DuplicatePerson = duplicatePerson;
+        }
+
+        public Person DuplicatePerson { get; protected set; }
+    }
 
     [DataContract]
     public class Composite1
