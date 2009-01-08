acab44e840d6e94394501875b6ac234c4c8ea136
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2009-01-08 19:54:18 +0000

    Completed service generation.

--- sl2/Wcf/Presentation/ServiceResult.cs
+++ sl2/Wcf/Presentation/ServiceResult.cs
@@ -1,10 +1,34 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Runtime.Serialization;
+using System.Windows.Threading;
 
 namespace SlWcf.Services
 {
+
+    public delegate void ServiceCallback(ServiceCallStatus status);
+
+    public class ServiceCallStatus
+    {
+        public object State { get; set; }
+        public ServiceCallback Callback { get; set; }
+        public IAsyncResult AsyncResult { get; set; }
+
+        public static void InvokeResultOnUiThread(  Dispatcher          dispatcher,
+                                                    ServiceCallStatus   serviceCallStatus)
+        {
+            if (dispatcher == null)
+            {
+                serviceCallStatus.Callback(serviceCallStatus);
+            }
+            else
+            {
+                dispatcher.BeginInvoke(serviceCallStatus.Callback, new object[] { serviceCallStatus });
+            }
+        }
+    }
 
     [DataContract]
     public class ServiceResult
@@ -14,6 +38,34 @@ namespace SlWcf.Services
         [DataMember] public string ExceptionMessage { get; set; }
         [DataMember] public string ExceptionClass { get; set; }
         [DataMember] public IDictionary<string, object> Properties { get; set; }
+
+        public void ProcessException()
+        {
+            if (!IsError)
+                return;
+
+            Type exceptionType = Type.GetType(ExceptionClass);
+
+            if (exceptionType == null)
+                throw new Exception("Unrecognised exception type (" + ExceptionClass + ")\r\n" + ExceptionMessage);
+
+            ConstructorInfo messageConstructor = exceptionType.GetConstructor(new Type[] { typeof(string) });
+            if (messageConstructor == null)
+                throw new Exception("No valid constructor taking a string for (" + ExceptionClass + ")\r\n" + ExceptionMessage);
+
+            Exception exception = (Exception) messageConstructor.Invoke(new object[] { ExceptionMessage });
+            foreach (string propertyName in Properties.Keys)
+            {
+                PropertyInfo property = exceptionType.GetProperty(propertyName);
+                if (property == null)
+                    throw new Exception("No property (" + propertyName + ") on (" + ExceptionClass + ")\r\n" + ExceptionMessage);
+
+                property.SetValue(exception, Properties[propertyName], null);
+            }
+
+            throw exception;
+        }
+
     }
 
     [DataContract]
