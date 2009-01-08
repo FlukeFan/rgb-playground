94dfca9f75b5b24a34a948fb2d735eb509bdf421
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2009-01-08 16:13:50 +0000

    Added initial generation of interface.

---
+++ sl2/Wcf/Presentation/ServiceResult.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace SlWcf.Services
+{
+
+    [DataContract]
+    public class ServiceResult
+    {
+        [DataMember] public bool IsVoid { get; set; }
+        [DataMember] public bool IsError { get; set; }
+        [DataMember] public string ExceptionMessage { get; set; }
+        [DataMember] public string ExceptionClass { get; set; }
+        [DataMember] public IDictionary<string, object> Properties { get; set; }
+    }
+
+    [DataContract]
+    public class ServiceResult<T> : ServiceResult
+    {
+        [DataMember]
+        public T Result { get; set; }
+    }
+
+}
+
