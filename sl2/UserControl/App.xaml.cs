5e9f4357ae7ea914ba252106199988057ba20bb3
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2009-02-16 23:18:42 +0000

    Placeholder project for user control.

---
+++ sl2/UserControl/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Browser;
+
+namespace SlUserControl
+{
+
+    public class App : System.Windows.Application
+    {
+
+        public App()
+        {
+            this.Startup += this.Application_Startup;
+            this.Exit += this.Application_Exit;
+            this.UnhandledException += this.Application_UnhandledException;
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Application.LoadComponent(this, new Uri("App.xaml", UriKind.Relative));
+            Uri uri = new Uri("Main.xaml", UriKind.Relative);
+
+            // Load the main control
+            Main main = new Main();
+            System.Windows.Application.LoadComponent(main, uri);
+            RootVisual = main;
+        }
+
+        private void Application_Exit(object sender, EventArgs e)
+        {
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString());
+        }
+
+    }
+}
---
+++ sl2/UserControl/Main.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SlUserControl
+{
+
+    public class Main : UserControl
+    {
+
+        private Button _test1;
+        private ScrollViewer _scrollViewer;
+        private StackPanel _messages;
+        private DataGrid _grid;
+
+        public Main()
+        {
+            Loaded += Main_Loaded;
+        }
+
+        private void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            _scrollViewer = (ScrollViewer)FindName("_scrollViewer");
+            _messages = (StackPanel)FindName("_messages");
+            _test1 = (Button)FindName("_test1");
+            _grid = (DataGrid)FindName("_grid");
+
+            Write("Loaded");
+        }
+
+        private void Write(string message)
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = DateTime.Now + " - " + message;
+            _messages.Children.Add(textBlock);
+            _scrollViewer.ScrollToVerticalOffset(_scrollViewer.ExtentHeight);
+        }
+
+    }
+}
