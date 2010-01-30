f5e366f576681adcad4d092451f9acee39a87113
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2010-01-30 12:28:40 +0000

    Checked in watin example placeholder.

---
+++ trunk/at/watin/WatinExample.cs
@@ -0,0 +1,15 @@
+
+using NUnit.Framework;
+
+namespace WatinExample
+{
+    [TestFixture]
+    public class WatinExample
+    {
+        [Test]
+        public void OpenBrowser()
+        {
+            Assert.Fail("Not implemented yet");
+        }
+    }
+}
