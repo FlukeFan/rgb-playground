7fd7bfea9da4fc6072b9952943937acaa52f2d43
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2010-02-01 08:49:26 +0000

    Fleshed out example.

--- trunk/at/watin/WatinExample.cs
+++ trunk/at/watin/WatinExample.cs
@@ -1,4 +1,5 @@
-
+using System;
+using System.Linq;
 using NUnit.Framework;
 using WatiN.Core;
 using Microsoft.VisualStudio.WebHost;
@@ -8,20 +9,69 @@ namespace WatinExample
     [TestFixture]
     public class WatinExample
     {
-        [Test]
-        public void OpenBrowser()
+        private Server _server;
+        private IE _ie;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetup()
         {
-            IE ie = new IE();
-
             string path = @"C:\work\rgb\playground\at\watin\site";
-            Server server = new Server(8181, "/", path);
-            server.Start();
+            _server = new Server(8181, "/", path);
+            _server.Start();
+        }
 
-            ie.GoTo("http://localhost:8181");
-            System.Threading.Thread.Sleep(5000);
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            _server.Stop();
+        }
 
-            ie.Dispose();
-            server.Stop();
+        [SetUp]
+        public void SetUp()
+        {
+            _ie = new IE(true);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _ie.Dispose();
+        }
+
+        protected void AssertCanSeeText(string text)
+        {
+            Console.WriteLine("Verify can see the text: " + text);
+            Assert.IsTrue(_ie.Body.OuterText.Contains(text), "Could not see text: " + text);
+        }
+
+        protected void OpenHomePage()
+        {
+            Console.WriteLine("Open the home page");
+            _ie.GoTo("http://localhost:8181");
+        }
+
+        protected void ClickButton(string buttonText)
+        {
+            Console.WriteLine("Click button: " + buttonText);
+            Button button = _ie.Buttons.Where(b => b.Value == buttonText).First();
+            button.Click();
+        }
+
+        [Test]
+        public void can_open_the_home_page()
+        {
+            OpenHomePage();
+
+            AssertCanSeeText("Hello");
+        }
+
+        [Test]
+        public void can_click_one()
+        {
+            OpenHomePage();
+            ClickButton("Click1");
+
+            AssertCanSeeText("Clicked");
         }
     }
 }
