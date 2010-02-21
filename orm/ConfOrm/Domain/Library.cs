60ab6a8fe0ad7d722abd4b3b24cda76023f507e6
FlukeFan <FlukeFan@d42988a8-254f-0410-95a8-fbcba849fc13>
2010-02-21 10:19:12 +0000

    Added property convention to demo.

--- orm/ConfOrm/Domain/Library.cs
+++ orm/ConfOrm/Domain/Library.cs
@@ -4,18 +4,19 @@ namespace MapDemo.Domain
 {
 	public abstract class Library : Base
 	{
-        public virtual string Name { get; protected set; }
+		public virtual string Name { get; protected set; }
+		public virtual string Description { get; protected set; }
 
-        public IEnumerable<Media> Media { get; protected set; }
+		public IEnumerable<Media> Media { get; protected set; }
 	}
 
-    public class SpecialLibrary : Library
-    {
-        public virtual string SpecialName { get; protected set; }
-    }
+	public class SpecialLibrary : Library
+	{
+		public virtual string SpecialName { get; protected set; }
+	}
 
-    public class DetailedLibrary : Library
-    {
-        public virtual string Detail { get; protected set; }
-    }
+	public class DetailedLibrary : Library
+	{
+		public virtual string Detail { get; protected set; }
+	}
 }
--- orm/ConfOrm/Domain/Media.cs
+++ orm/ConfOrm/Domain/Media.cs
@@ -2,18 +2,19 @@ using System.Collections.Generic;
 
 namespace MapDemo.Domain
 {
-    public enum MediaType
-    {
-        Book = 1,
-        Dvd = 2,
-        CD = 3,
-    }
-
-    public class Media : Base
+	public enum MediaType
 	{
-        public Library OwningLibrary { get; protected set; }
+		Book = 1,
+		Dvd = 2,
+		CD = 3,
+	}
 
-        public MediaType Type { get; protected set; }
-        public string Title { get; protected set; }
+	public class Media : Base
+	{
+		public virtual Library OwningLibrary { get; protected set; }
+
+		public virtual MediaType Type { get; protected set; }
+		public virtual string Title { get; protected set; }
+		public virtual string Description { get; protected set; }
 	}
 }
